e2a9ca4d9030aef53f0b5b99b4db747829b343b7
DJ <dayanandjagtap07@gmail.com>
2023-12-30 21:27:55 +0530

    solve ProjectGrid import bug

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,6 +1,6 @@
 import "@/styles/project.css";
 import { useState } from "react";
-import ProjectGrid from "./ProjectHelpers/projectGrid";
+import ProjectGrid from "./ProjectHelpers/ProjectGrid";
 
 const miscProjects = [
     {
@@ -97,50 +97,5 @@ const ProjectSection = () => {
         </section>
     );
 };
-
-// const ProjectGrid = ({ projectList }) => {
-//     return (
-//         <div className="project-content">
-//             {projectList.map((e) => {
-//                 return (
-//                     <div
-//                         key={e.imageUrl}
-//                         className="content-card flex flex-col justify-center align-start"
-//                     >
-//                         <div className="content-cardLink flex justify-center align-center">
-//                             <a
-//                                 href={e.projectLink}
-//                                 target="_blank"
-//                                 className="flex center"
-//                             >
-//                                 <FaCode size={24} />
-//                             </a>
-//                             {e.websiteLink && (
-//                                 <a
-//                                     href={e.websiteLink}
-//                                     target="_blank"
-//                                     className="flex center"
-//                                 >
-//                                     <FaLink size={20} />
-//                                 </a>
-//                             )}
-//                         </div>
-//                         <Image
-//                             src={e.imageUrl}
-//                             width={400}
-//                             height={400}
-//                             alt={e.title}
-//                             className="content-cardImage"
-//                         />
-//                         <article className="content-cardText">
-//                             <h3>{e.title}</h3>
-//                             <p>{e.description}</p>
-//                         </article>
-//                     </div>
-//                 );
-//             })}
-//         </div>
-//     );
-// };
 
 export default ProjectSection;
